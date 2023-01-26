64bf76a20b4ea3cd8ccaede178a16b543bb47289
idkrn <damik1318@gmail.com>
2023-01-26 23:37:33 +0100

    Updates

---
+++ precache-manifest.e410cc2d7120e09c567e543e3a7b909c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05cacbe0723ebd709c89",
+    "url": "./deadcellsmap/static/css/main.879d4c9d.chunk.css"
+  },
+  {
+    "revision": "05cacbe0723ebd709c89",
+    "url": "./deadcellsmap/static/js/main.05cacbe0.chunk.js"
+  },
+  {
+    "revision": "74057af0f6665da8b728",
+    "url": "./deadcellsmap/static/js/1.74057af0.chunk.js"
+  },
+  {
+    "revision": "89599fd6805ac26cbc27",
+    "url": "./deadcellsmap/static/js/runtime~main.89599fd6.js"
+  },
+  {
+    "revision": "2d1f031b35509f34ee1129615ccdf4a3",
+    "url": "./deadcellsmap/index.html"
+  }
+];
