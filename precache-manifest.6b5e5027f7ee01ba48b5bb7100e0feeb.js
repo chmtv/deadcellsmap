d2f464ad65cf4813a7341921fab818eba38d7323
chmtv <damik1318@gmail.com>
2024-04-23 23:34:15 +0200

    Updates

--- precache-manifest.6b5e5027f7ee01ba48b5bb7100e0feeb.js
+++ precache-manifest.6b5e5027f7ee01ba48b5bb7100e0feeb.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "96848433a7682927bec5",
-    "url": "/deadcellsmap/static/css/main.72c14e10.chunk.css"
+    "revision": "b66d44ab05d69da5ec87",
+    "url": "/deadcellsmap/static/css/main.7c67c415.chunk.css"
   },
   {
-    "revision": "96848433a7682927bec5",
-    "url": "/deadcellsmap/static/js/main.96848433.chunk.js"
+    "revision": "b66d44ab05d69da5ec87",
+    "url": "/deadcellsmap/static/js/main.b66d44ab.chunk.js"
   },
   {
-    "revision": "737b0bc184e71ad6fcb2",
-    "url": "/deadcellsmap/static/js/1.737b0bc1.chunk.js"
+    "revision": "286a8ceb1abd037edd26",
+    "url": "/deadcellsmap/static/js/1.286a8ceb.chunk.js"
   },
   {
     "revision": "a5018043c1289868ad79",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/deadcellsmap/static/media/Dosis-VariableFont_wght.d89d7b6e.ttf"
   },
   {
-    "revision": "e7b9d580cb632ab4cd81babee09e4ffe",
+    "revision": "7d5e5c9a5beec9d076cb1fbf2a41801c",
     "url": "/deadcellsmap/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deadcellsmap/precache-manifest.049c80e67e394bbacfa23bbf9afe4860.js"
+  "/deadcellsmap/precache-manifest.6b5e5027f7ee01ba48b5bb7100e0feeb.js"
 );
 
 workbox.clientsClaim();
